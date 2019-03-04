19e9bfbe768a4eb8bc700506ade00e41c8d30d3a
alch <epsilon_chi@outlook.com>
2019-03-04 12:34:50 -0800

    New extractor code

---
+++ support/extractor/extractor.ino
@@ -0,0 +1,55 @@
+#define SD_FILE "WRITE.TXT"
+#include <SD.h>
+
+typedef struct{
+	unsigned char FLAGS;
+	unsigned long SD_ADDR;
+	unsigned long time;
+	byte NFF[204];
+	int SENSE[4];
+	int AV[16][9];
+	int FLOW;
+	unsigned char nul = 0x0;
+} DATA;
+
+File f;
+DATA d;
+byte* db = &d;
+
+void setup(){
+	SD.begin();
+	f = SD.open(SD_FILE, FILE_READ);
+}
+
+void loop(){
+	while(f.availible){
+		f.read(db, 512);
+		Serial.println("+++++");
+		Serial.print("FLAGS: ");
+		Serial.println(d.FLAGS);
+		Serial.print("Time: ");
+		Serial.println(d.time);
+		Serial.print("NFF DATA: ");
+		for(int i = 0; i < 204; i++){
+			Serial.print(char(d.NFF[i]));
+		}
+		Serial.println();
+		Serial.print("SENSE DATA: ");
+		for(int i = 0; i < 4; i++){
+			Serial.print(d.SENSE[i]);
+			Serial.print(" ");
+		}
+		Serial.println();
+		Serial.print("AV DATA: ");
+		for(int i = 0; i < 16; i++){
+			for(int j = 0; j < 9; j++){
+				Serial.print(d.AV[i][j]);
+				Serial.print(" ");
+			}
+			Serial.println();
+		}
+		Serial.print("FLOW: ");
+		Serial.println(d.FLOW);
+		Serial.println("-----");
+	}
+}
