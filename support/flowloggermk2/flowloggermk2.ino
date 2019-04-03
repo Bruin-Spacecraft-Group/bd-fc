bb018d4db94c21989ca786dbe2f5d4e513aa3476
Andrew <andrewe456@gmail.com>
2019-04-02 21:14:48 -0700

    some change during testing?

--- support/flowloggermk2/flowloggermk2.ino
+++ support/flowloggermk2/flowloggermk2.ino
@@ -1,41 +1,58 @@
 #include <Wire.h>
-#include <Adafruit_INA219.h>
 
-Adafruit_INA219 cur_sense;
+//Adafruit_INA219 cur_sense;
 int freq;
-float shuntvoltage = 0;
-float busvoltage = 0;
-float current_mA = 0;
-float loadvoltage = 0;
-float power_mW = 0;
+int16_t busvoltage = 0;
+int16_t current_mA = 0;
 
 void setup(){
 	Serial.begin(115200);
-	cur_sense.setCalibration_32V_1A();
 	Wire.begin();
+  Serial.println("thing");
+	//cur_sense.setcalibration_32v_1a();
+	Wire.beginTransmission(0x40);
+	Wire.write(0x05);
+	Wire.write((10240)>>8 & 0xFF);
+	Wire.write((10240)    & 0xFF);
+	Wire.endTransmission();
+	Wire.beginTransmission(0x40);
+	Wire.write(0x00);
+	Wire.write((14751)>>8 & 0xFF);
+	Wire.write((14751)    & 0xFF);
+	Wire.endTransmission();
 	Serial.println("Ready!");
 }
 
 void loop(){
-	shuntvoltage = cur_sense.getShuntVoltage_mV();
-	busvoltage = cur_sense.getBusVoltage_V();
-	current_mA = cur_sense.getCurrent_mA();
-	power_mW = cur_sense.getPower_mW();
-	loadvoltage = busvoltage + (shuntvoltage / 1000);
+//	shuntvoltage = cur_sense.getShuntVoltage_mV();
+//	busvoltage = cur_sense.getBusVoltage_V();
+//	current_mA = cur_sense.getCurrent_mA();
+//	power_mW = cur_sense.getPower_mW();
+//	loadvoltage = busvoltage + (shuntvoltage / 1000);
 
-	Wire.beginTransmission(0x0F);	
-	Wire.write(0);
-	Wire.beginTransmission(0x0F);	
+	Wire.requestFrom(0x07 ,2);
+	freq = ((Wire.read() << 8) | Wire.read()) / 10;
 
-	Wire.requestFrom(0x0F ,2);
-	if (Wire.available())
-		freq = ((Wire.read() << 8) | Wire.read()) / 10;
+	Wire.beginTransmission(0x40);
+	Wire.write(0x04);
+	Wire.endTransmission();
+	delay(1);
+	Wire.requestFrom(0x40, 2);
+	current_mA = ((Wire.read() << 8) | Wire.read());
+
+	Wire.beginTransmission(0x40);
+	Wire.write(0x02);
+	Wire.endTransmission();
+	delay(1);
+	Wire.requestFrom(0x40, 2);
+	busvoltage = ((Wire.read() << 8) | Wire.read());
+	busvoltage = (busvoltage >> 3) * 4;
 
 	Serial.print(millis());
 	Serial.print(" Current: ");
-	Serial.print(current_mA);
+	Serial.print((float)current_mA / 25.0);
 	Serial.print(" Bus Voltage: ");
-	Serial.print(busvoltage);
+	Serial.print(busvoltage * 0.001);
 	Serial.print(" Flow (SCCM): ");
 	Serial.println(freq);
 	delay(500);
